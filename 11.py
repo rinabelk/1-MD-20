820df578bc9c0c961e6769dea741639526c3f0a3
rinabelk <159255465+rinabelk@users.noreply.github.com>
2024-05-22 00:02:46 +0300

    Create 11.py
    ----

---
+++ 11.py
@@ -0,0 +1,28 @@
+class Restaurant:
+
+    def __init__(restor, restaurant_name, cuisine_type):
+        restor.restaurant_name = restaurant_name
+        restor.cuisine_type = cuisine_type
+
+    def describe_restaurant(restor):
+        print(f"Название ресторана: {restor.restaurant_name}")
+        print(f"Тип кухни: {restor.cuisine_type}")
+
+    def open_restaurant(restor):
+        print(f"{restor.restaurant_name} открыт!")
+
+
+
+newRestaurant = Restaurant("Марчелиз", "итальянская")
+
+newRestaurant.describe_restaurant()
+newRestaurant.open_restaurant()
+
+resta1 = Restaurant("Токио Сити", "кухни всех видов")
+resta2 = Restaurant("ДедХо", "въетнамская")
+resta3 = Restaurant("Korean", "корейская")
+
+# Вызываем для каждого экземпляра метод describe_restaurant()
+resta1.describe_restaurant()
+resta2.describe_restaurant()
+resta3.describe_restaurant()
